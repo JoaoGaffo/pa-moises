f7787f0e70c01c90eaf725dd943c5ecc4994b3f9
aluno <e023aluno@outlook.com>
2026-03-04 21:39:15 -0300

    variaveis0403

---
+++ aula_04-03/variaveis.tsx
@@ -0,0 +1,11 @@
+const numeroUm = 4
+
+const numeroDois = 2
+
+const soma = (numeroUm + numeroDois)
+const sub = (numeroUm - numeroDois)
+const div = (numeroUm / numeroDois)
+const multi = (numeroUm * numeroDois)
+
+console.log(soma, sub, div, multi)
+
